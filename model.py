ece2d8fc9b24998a7c091b4a4c283f2a605f0d05
Bruno Korbar <bkorbar@fb.com>
2019-12-23 11:07:06 -0800

    Allow support for r25d trained on IG

--- model.py
+++ model.py
@@ -3,6 +3,8 @@ import torch as th
 import torchvision.models as models
 from videocnn.models import resnext
 from torch import nn
+sys.path.insert(0, "/private/home/bkorbar/torch_projects/fe_h21M/")
+from models.video_classification import r2plus1d_152, r2plus1d_34
 
 
 class GlobalAvgPool(nn.Module):
@@ -12,15 +14,23 @@ class GlobalAvgPool(nn.Module):
     def forward(self, x):
         return th.mean(x, dim=[-2, -1])
 
+class AdaptivePool(nn.Module):
+    def __init__(self, d=2):
+        super(AdaptivePool, self).__init__()
+        self.pool = nn.AdaptiveAvgPool3d((1, d, d))
+    
+    def forward(self, x):
+        return self.pool(x).view(-1, 2048)
+
 
 def get_model(args):
-    assert args.type in ['2d', '3d']
+    assert args.type in ['2d', '3d', 'ig']
     if args.type == '2d':
         print('Loading 2D-ResNet-152 ...')
         model = models.resnet152(pretrained=True)
         model = nn.Sequential(*list(model.children())[:-2], GlobalAvgPool())
         model = model.cuda()
-    else:
+    elif args.type == "3d":
         print('Loading 3D-ResneXt-101 ...')
         model = resnext.resnet101(
             num_classes=400,
@@ -32,6 +42,12 @@ def get_model(args):
         model = model.cuda()
         model_data = th.load(args.resnext101_model_path)
         model.load_state_dict(model_data)
+    else:
+        model = r2plus1d_34()
+        checkpoint = th.load(args.ig_model_path, map_location='cpu')
+        model.load_state_dict(checkpoint['model'])
+        model = th.nn.Sequential(*list(model.children())[:-2], AdaptivePool())
+        model = model.cuda()
 
     model.eval()
     print('loaded')
